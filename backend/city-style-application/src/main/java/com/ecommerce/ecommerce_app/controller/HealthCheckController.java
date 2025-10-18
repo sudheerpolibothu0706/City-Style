77d16779579f4faa87e65f4cd1673e9bb2c85cdc
Polibothu Sudheer <144912892+sudheerpolibothu0706@users.noreply.github.com>
2025-10-18 12:59:09 +0530

    Create HealthCheckController.java

---
+++ backend/city-style-application/src/main/java/com/ecommerce/ecommerce_app/controller/HealthCheckController.java
@@ -0,0 +1,11 @@
+package com.ecommerce.ecommerce_app.controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "OK";
+    }
+}
