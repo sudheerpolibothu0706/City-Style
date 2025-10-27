e5fac7a07d1de4bbdde96fb76d5537247f0238cf
Polibothu Sudheer <144912892+sudheerpolibothu0706@users.noreply.github.com>
2025-10-27 23:51:19 +0530

    Update OrderRepository.java

--- backend/city-style-application/src/main/java/com/ecommerce/ecommerce_app/repository/OrderRepository.java
+++ backend/city-style-application/src/main/java/com/ecommerce/ecommerce_app/repository/OrderRepository.java
@@ -8,6 +8,6 @@ import com.ecommerce.ecommerce_app.model.User;
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    // Allows users to view their order history
+    
     List<Order> findByUser(User user);
 }
